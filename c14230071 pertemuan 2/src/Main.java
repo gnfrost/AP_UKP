811c6a3dcd0b9324824094722703df81ace54ff7
john <john@doe.com>
2023-08-09 14:20:14 +0700

    init

---
+++ c14230071 pertemuan 2/src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+    System.out.println("---Data Mahasiswa---");
+    System.out.println("Nama : Timothy Sebastian Itamurti");
+    System.out.println("NRP : c14230071");
+    System.out.println("Mata Kuliah : Algoritma dan Pemrograman");
+    System.out.println("Kelas : B");
+    System.out.println("Tahun : 2023/2024");
+    System.out.println("Semester : Gasal");
+    System.out.println("--------------------");
+    }
+}
